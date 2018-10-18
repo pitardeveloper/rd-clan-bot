f9724fb04e91a80cc053d650520c2795adefae32
pitardeveloper <37588350+pitardeveloper@users.noreply.github.com>
2018-10-18 03:59:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,42 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+client.on('ready',  () => {
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+  console.log('CREATED BY : PITAR');
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+  console.log(`Logged in as  * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+  console.log('⚙            https://discord.gg/2yCdFZ | سيرفر الدعم');
+  console.log('⚙            BY : PITAR');
+client.user.setStatus("dnd");
 });
+                                                                                                                                                                                              var prefix = "!";
 
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command === "Bb") {
+    let numArray = args.map(n=> parseInt(n));
+    let total = numArray.reduce( (p, c) => p+c);
+
+    message.channel.sendMessage(total).catch(console.error);
+  }
+  
+  
+
+  });
+
+// playing
+client.on('ready', () => {
+                                                                                                                                                                                                                                                                              client.user.setGame(`!help | ArabicoBot`,'https://www.twitch.tv/v5bz');
+});
 client.on('message', msg => {
   if (msg.content === 'باك') {
     msg.reply('**ولكم منور يا عسل**');
